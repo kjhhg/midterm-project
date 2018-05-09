dec3d9b85e3e7e0403c8620068c29079635a1578
kjhhg <eagle870608@gmail.com>
2018-05-09 13:45:41 +0800

    Update InfoScreen.js

--- screens/InfoScreen.js
+++ screens/InfoScreen.js
@@ -15,17 +15,17 @@ class InfoScreen extends Component {
                     <View style = {[user, {flex: 1}]}><Text style = {{fontSize: 12}}>個人頭像</Text></View>
                     <View style = {[user, {flex: 1}]}><Text style = {{fontSize: 12}}>10K/15K</Text></View>
                 </View>
-                <View style = {{flex: 1}}>
-                    <View><Text style = {titleDown}>帳號</Text></View>
-                    <View><Text style = {box}>alan_walkr</Text></View>
-                    <View><Text style = {titleDown}>電子信箱</Text></View>
-                    <View><Text style = {box}>xxxxxxxxx.gmail.com</Text></View>
-                    <View><Text style = {titleDown}>獲得能量次數</Text></View>
-                    <View><Text style = {box}>500</Text></View>
-                    <View><Text style = {titleDown}>活動參與次數</Text></View>
-                    <View><Text style = {box}>10</Text></View>
-                    <View><Text style = {titleDown}>最長持續天數</Text></View>
-                    <View><Text style = {box}>100</Text></View>
+               View style = {[{flex:1},{alignItems:  'center'}]}>
+                    <View style = {{flex: 1}}><Text style = {titleDown}>帳號</Text></View>
+                    <View style = {{flex: 1}}><Text style = {box}>alan_walkr</Text></View>
+                    <View style = {{flex: 1}}><Text style = {titleDown}>電子信箱</Text></View>
+                    <View style = {{flex: 1}}><Text style = {box}>xxxxxxxxx.gmail.com</Text></View>
+                    <View style = {{flex: 1}}><Text style = {titleDown}>獲得能量次數</Text></View>
+                    <View style = {{flex: 1}}><Text style = {box}>500</Text></View>
+                    <View style = {{flex: 1}}><Text style = {titleDown}>活動參與次數</Text></View>
+                    <View style = {{flex: 1}}><Text style = {box}>10</Text></View>
+                    <View style = {{flex: 1}}><Text style = {titleDown}>最長持續天數</Text></View>
+                    <View style = {{flex: 1}}><Text style = {box}>100</Text></View>
                 </View>
             </View>
         )
